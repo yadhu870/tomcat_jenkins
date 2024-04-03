2e8cac70c5753a477f0c470437945d3551f16263
yadhu <yadhu870@gmail.com>
2024-04-03 17:07:22 +0000

    tomcat and jenkins inatallation + configuration

---
+++ tomcat/tomcat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+cd /tmp
+curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
+sudo mkdir /opt/tomcat
+sudo tar xzvf apache-tomcat-*tar.gz -C /opt/tomcat --strip-components=1
+cd /opt/tomcat
+sudo chgrp -R tomcat /opt/tomcat
+sudo chmod -R g+r conf
+sudo chmod g+x conf
+sudo chown -R tomcat webapps/ work/ temp/ logs/
+sudo update-java-alternatives -l
+sudo systemctl daemon-reload
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl status tomcat
